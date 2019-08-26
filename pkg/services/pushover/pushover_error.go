7b77ebceb94762465179db654638fcf2a81d4f24
Simon Aronsson <simme@arcticbit.se>
2019-08-26 21:59:14 +0200

    fix codacy warnings

--- pkg/services/pushover/pushover_error.go
+++ pkg/services/pushover/pushover_error.go
@@ -4,6 +4,8 @@ package pushover
 type ErrorMessage string
 
 const (
+	// UserMissing should be used when a config URL is missing a user
 	UserMissing ErrorMessage = "user missing from config URL"
+	// TokenMissing should be used when a config URL is missing a token
 	TokenMissing ErrorMessage = "token missing from config URL"
 )
