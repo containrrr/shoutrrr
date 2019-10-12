057ce0d86e1fce48df5860a4eef4a535df7aea67
Simon Aronsson <simme@arcticbit.se>
2019-10-12 14:11:51 +0200

    fix(util): improve test coverage

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1,6 +1,7 @@
 package util_test
 
 import (
+	"reflect"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -38,6 +39,32 @@ var _ = Describe("the util package", func() {
 		})
 		It("should have the prefix \"Test\"", func() {
 			Expect(TestLogger().Prefix()).To(Equal("Test"))
+		})
+	})
+	When("checking if a supplied kind is of the signed decimal kind", func() {
+		It("should be true if the kind is Int", func() {
+			Expect(IsSignedDecimal(reflect.Int)).To(BeTrue())
+		})
+		It("should be false if the kind is String", func() {
+			Expect(IsSignedDecimal(reflect.String)).To(BeFalse())
+		})
+	})
+
+	When("checking if a supplied kind is of the unsigned decimal kind", func() {
+		It("should be true if the kind is Uint", func() {
+			Expect(IsUnsignedDecimal(reflect.Uint)).To(BeTrue())
+		})
+		It("should be false if the kind is Int", func() {
+			Expect(IsUnsignedDecimal(reflect.Int)).To(BeFalse())
+		})
+	})
+
+	When("checking if a supplied kind is of the collection kind", func() {
+		It("should be true if the kind is slice", func() {
+			Expect(IsCollection(reflect.Slice)).To(BeTrue())
+		})
+		It("should be false if the kind is map", func() {
+			Expect(IsCollection(reflect.Map)).To(BeFalse())
 		})
 	})
 })
