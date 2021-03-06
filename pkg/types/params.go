20d5656807d972041c76b3c1468955bcdbb2811c
nils måsén <nils@piksel.se>
2021-03-06 16:49:20 +0100

    feat(format): improve default props handling
    this makes it possible to ignore errors setting
    config props and defaults

--- pkg/types/params.go
+++ pkg/types/params.go
@@ -3,7 +3,10 @@ package types
 // Params is the string map used to provide additional variables to the service templates
 type Params map[string]string
 
-const titleKey = "title"
+const (
+	titleKey   = "title"
+	messageKey = "message"
+)
 
 // SetTitle sets the "title" param to the specified value
 func (p Params) SetTitle(title string) {
@@ -14,4 +17,9 @@ func (p Params) SetTitle(title string) {
 func (p Params) Title() (title string, found bool) {
 	title, found = p[titleKey]
 	return
+}
+
+// SetMessage sets the "message" param to the specified value
+func (p Params) SetMessage(message string) {
+	p[messageKey] = message
 }
