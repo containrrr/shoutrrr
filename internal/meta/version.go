bbe8fe2a15097482fb8b6976b455ff6afc9a60e8
nils måsén <nils@piksel.se>
2021-08-31 22:09:51 +0200

    update meta for v0.5.1

--- internal/meta/version.go
+++ internal/meta/version.go
@@ -1,7 +1,7 @@
 package meta
 
 // Version of Shoutrrr
-const Version = `0.5`
+const Version = `0.5.1`
 
 // DocsVersion is prepended to documentation URLs and usually equals MAJOR.MINOR of Version
 const DocsVersion = `v0.5`
