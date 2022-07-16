799a8ceb9b423906f6d4300b9c75cb7d54c3d247
nils måsén <nils@piksel.se>
2022-07-16 14:31:21 +0200

    bump dev version

--- internal/meta/version.go
+++ internal/meta/version.go
@@ -1,7 +1,7 @@
 package meta
 
 // Version of Shoutrrr
-const Version = `0.5-dev`
+const Version = `0.6-dev`
 
 // DocsVersion is prepended to documentation URLs and usually equals MAJOR.MINOR of Version
 const DocsVersion = `dev`
