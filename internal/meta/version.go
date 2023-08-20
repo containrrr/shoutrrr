20dfd4d47ecb9478696b4a730a5465a2f9e5cf13
nils måsén <nils@piksel.se>
2023-08-20 17:33:47 +0200

    release v0.8.0

--- internal/meta/version.go
+++ internal/meta/version.go
@@ -1,7 +1,7 @@
 package meta
 
 // Version of Shoutrrr
-const Version = `0.7.1`
+const Version = `0.8.0`
 
 // DocsVersion is prepended to documentation URLs and usually equals MAJOR.MINOR of Version
-const DocsVersion = `v0.7`
+const DocsVersion = `v0.8`
